ae15148232a55d73e5bc564ced82e081062a4599
Bruno Leão Guimarães <bl.guimaraes@gmail.com>
2023-01-24 15:09:07 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// my headerparser API endpoint
+app.get('/api/whoami', function (req, res) {
+  
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
